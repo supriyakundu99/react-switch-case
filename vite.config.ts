8deef1b6ff211f8028877045b0aca4305e2809ca
Supriya Kundu <supriya.kundu.1999@gmail.com>
2024-03-09 22:51:59 +0530

    Used vit tu publish

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
+import path from 'path';
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/components/index.tsx'),
+      name: 'SwitchCase',
+      fileName: (format) => `react-switch-case-statement.${format}.js`
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React'
+        }
+      }
+    }
+  },
+  plugins: [react()]
 })
